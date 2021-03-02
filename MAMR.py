c5ab89d1cee4a8dc5480e2701dafc4a5275d3a73
XY-1 <48458244+XY-1@users.noreply.github.com>
2021-03-02 14:36:53 +0900

    Update MAMR.py

--- MAMR.py
+++ MAMR.py
@@ -7,123 +7,13 @@ import numpy as np
 
 #need to run data/get_candles_spot.py
 DATABASE = 'data/candles_12h.db'
-#CURRENCIES = ['BTC', 'ETH', 'EOS', 'LTC', 'BNB', 'XRP', 'BCH', 'ADA', 'XMR']
+CURRENCIES = ['BTC', 'ETH', 'EOS', 'LTC', 'BNB', 'XRP', 'BCH', 'ADA', 'XMR']
+#'USDC', 'UNI', 'DOGE', 'XEM', 'AAVE', 'ATOM']
+#'SOL','TRX', 'THETA', 'XTZ', 'VET', 'NEO', 'LUNA', 'BUSD', 'EGLD', 'ALGO',
+#'SNX', 'DASH', 'FIL', 'SUSHI', 'MKR', 'AVAX', 'KSM', 'COMP', 'DCR',
+#'ZEC', 'ETC', 'ZIL', 'BTT', 'CAKE', 'FTM', 'NEAR', 'RVN', 'YFI', 'UMA',
+#'LSK', 'BAT', 'ENJ', 'IOST', 'QTUM', 'GRT']
 #CURRENCIES = ['BTC', 'ETH', 'EOS', 'LTC', 'BNB', 'BCH', 'ADA']
-CURRENCIES = ['ETH', 'EOS', 'LTC', 'BNB', 'BCH', 'ADA' ,'XRP','LINK','XLM','DOGE','ALGO','ATOM','TRX','XEM','IOTA','VET']
-#CURRENCIES = ['ETH', 'EOS', 'FTT', 'LTC', 'BCH', 'ADA', 'XRP', 'XMR', 'LSK', 'FCT', 'XEM', 'MONA', 'XLM', 'QTMU', 'BAT', 'IOST', 'ENJ', 'DOGE', 'DOT', 'LINK', 'ATOM', 'TRX', 'IOTA' ,'VET', 'XTZ', 'THETA', 'NEO', 'FTT', 'MKR', 'DASH', 'ETC', 'ZRX', 'DCR', 'ZIL', 'WAVES' ,'SC', 'AE', 'MANA','NANO','ONT','ZEC','DGB','OMG','HBAR','ICX','ZEN','KNC','BNT','RVN','OCEAN','MATIC']
-
-#Quote Assets
-QUOTES = ['BTC', 'BNB']
-RISKLESS = 'USDT'
-
-
-DATABASE = 'data/candles_12h.db'
-
-class binanceBot:
-	def __init__(self, api, secret, saved=None, n=26):
-		self.account = account(api, secret)
-		self.balances = np.array([self.account.balances[c] if c in self.account.balances else 0.0 for c in [RISKLESS] + QUOTES + CURRENCIES])
-
-		
-
-		if saved is not None:
-			try:
-				with open(saved, 'rb') as file:
-					loaded = pickle.load(file)
-					self.manager = loaded.manager
-					self.prices = loaded.prices
-					self.returns = loaded.returns
-					self.update_times = loaded.update_times
-					self.portfolio = np.array(loaded.portfolio)
-					return
-
-			except FileNotFoundError:
-				pass
-
-		#get the portfolio...
-		self.manager = MAMRPortfolioManager(len(CURRENCIES) + len(QUOTES) + 1, 4.105, 9.5, 1000, 0.0, n)
-		
-
-		from data.get_candles_spot import main as get_candles
-		get_candles()
-
-		
-
-		price_changes = []
-		times = []
-		self.prices = []
-		
-		for candle in candleLoader(DATABASE):
-			#consider markets trading against BTC, so we need to invert the USDT price
-			self.prices.append(np.array([1] + [candle[currency + 'USDT_OPEN'] for currency in QUOTES + CURRENCIES]))
-			if len(self.prices) == 1:
-				continue
-			else:
-				price_changes.append(self.prices[-1] / self.prices[-2])
-				times.append(candle['open_time'])
-
-		for change, time in zip(price_changes[-n:], times[-n:]):
-			self.manager.update(time, change)
-
-
-		self.returns = price_changes
-		self.update_times = times
-		self.portfolio = self.account.get_portfolio_weighted(['USDT'] + QUOTES + CURRENCIES)
-		
-		self.manager.portfolio = np.array(self.portfolio)
-		self.prices.append(np.array([1.05 ** (0.5 / 365)] + [np.mean(market.prices([a + 'USDT' for a in QUOTES + CURRENCIES])[b + 'USDT']) for b in QUOTES + CURRENCIES]))
-
-	def save(self, location):
-		with open(location, 'wb') as file:
-			pickle.dump(self, file)
-
-	def update(self):
-		market_prices =market.prices([a + 'USDT' for a in QUOTES + CURRENCIES])
-		#self.prices.append(np.array([1.05 ** (0.5 / 365)] + [np.mean(market_prices[b + 'USDT']) for b in QUOTES + CURRENCIES]))
-		self.prices.append(np.array([1] + [np.mean(market_prices[b + 'USDT']) for b in QUOTES + CURRENCIES]))
-		returns = self.prices[-1] / self.prices[-2]
-		returns[0] = 1.05 ** (0.5 / 365) #should get realtime returns
-
-		self.returns.append(returns)
-		self.update_times.append(int(datetime.datetime.now().timestamp() * 1000))
-		usd_balances = self.balances * self.prices[-1]
-
-		self.manager.update(self.update_times[-1], self.returns[-1])
-
-		
-		target_portfolio = np.array(self.manager.portfolio)
-		#swutch bnb abd usdt to get fee reductions
-		usdt_proportion = target_portfolio[0]
-		target_portfolio[0] = target_portfolio[2]
-		target_portfolio[2] = usdt_proportion
-		
-		#The prices in BNB will be aquired in the trade method, maybe should just get these originally... 
-		self.account.trade_to_portfolio('BNB', ['BNB', 'BTC', 'USDT'], CURRENCIES, target_portfolio)
-
-
-		self.portfolio = self.account.get_portfolio_weighted(['USDT'] + QUOTES + CURRENCIES)
-		print(self.portfolio, self.manager.portfolio)
-		
-		with open('output.txt', 'a') as file:
-			file.write(str(self.manager.portfolio) + str(self.portfolio))
-
-
-def main():
-
-
-	try:
-		import keys
-	except ModuleNotFoundError:
-		print('keys.py file missing - see readme for set up instructions')
-		return 
-
-	bot = binanceBot(keys.API, keys.SECRET, 'state.pkl')
-	
-	bot.update()
-	bot.save('state.pkl')
-
-if __name__ == '__main__':
-	main()
 
 def main():
 	#get all the candles and calculate the price changes
@@ -143,14 +33,23 @@ def main():
 
 	#manager = MAMRPortfolioManager(len(CURRENCIES) + 1, 11.75, 371, 995, 0.001, 23)
 	#manager = MAMRPortfolioManager(len(CURRENCIES) + 1, 4.105, 9.5, 1000, 0.00075, 26)
-	manager = MAMRPortfolioManager(len(CURRENCIES) + 1, 4.105, 9.5, 1000, 0.000, 26)
+	manager = MAMRPortfolioManager(len(CURRENCIES) + 1, 2.105, 9.5, 1000, 0.000, 3)
+	#manager2 = MAMRPortfolioManager(len(CURRENCIES) + 1, 2.105, 9.5, 1000, 0.000, 15)
+	manager3 = MAMRPortfolioManager(len(CURRENCIES) + 1, 2.105, 9.5, 1000, 0.000, 26)
+	manager4 = MAMRPortfolioManager(len(CURRENCIES) + 1, 2.105, 9.5, 1000, 0.000, 45)
 	#manager = MAMRPortfolioManager(len(CURRENCIES) + 1, 50, 50, 60, 0.000, 26)
 
 	for change, time in zip(price_changes, times):
+		manager.value=(manager.value+manager3.value+manager4.value)/3
+		#manager2.value=(manager.value+manager2.value+manager3.value+manager4.value)/3
+		manager3.value=(manager.value+manager3.value+manager4.value)/3
+		manager4.value=(manager.value+manager3.value+manager4.value)/3
 		manager.update(time, change)
+		#manager2.update(time, change)
+		manager3.update(time, change)
+		manager4.update(time, change)
 
-
-	print(manager.value)
+	print((manager.value+manager3.value+manager4.value)/3)
 	
 	
 	plt.figure(0)
